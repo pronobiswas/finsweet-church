b86c9ee073e39938d94523d85f95dff2b3893464
PRONOB BISWAS <biswaspronob@outlook.com>
2023-12-30 02:30:52 +0600

    nav and banner statcure have done but responsive not yet

---
+++ taitwind.config.js
@@ -0,0 +1,16 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          clifford: '#da373d',
+          bg: '#0D0D0D',
+          btnbg: '#FFD2A4'
+        },
+        backgroundImage: {
+            'bannerbg': "url('./images/banner-bg.jpg')",
+            'footer-texture': "url('/img/footer-texture.png')",
+          }
+    
+      }
+    }
+  }
