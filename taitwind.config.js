37879e7feb98da5bd122a51c2b571c5f002fca61
PRONOB BISWAS <biswaspronob@outlook.com>
2023-12-31 03:52:42 +0600

    the benefit of our church section is done with responsive and hover effect but previous sections hover effect is not done like button hover effect

--- taitwind.config.js
+++ taitwind.config.js
@@ -6,9 +6,14 @@ tailwind.config = {
           bg: '#0D0D0D',
           btnbg: '#FFD2A4'
         },
+        fontFamily: {
+          roboto: 'Roboto Condensed'
+          
+        },
         backgroundImage: {
             'bannerbg': "url('./images/banner-bg.jpg')",
             'footer-texture': "url('/img/footer-texture.png')",
+            'overlay': 'linear-gradient(40deg, rgba(6, 6, 6, 0.76) 16.68%, rgba(0, 0, 0, 0.07) 96.44%);'
           }
     
       }
